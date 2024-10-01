60e77766f0e63ae87c6ea4e096de4ba6e92a1f1a
abhishekgupta3nbcu <abhig.ued@gmail.com>
2024-10-01 17:31:03 +0100

    New Functions (postcode API call)

---
+++ OneDrive_1_26-09-2024/Lon_Lat.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import urllib.request
+import json
+import sqldf
+
+def get_data_from_lon_lat(lon, lat):
+    
+    url = "https://api.postcodes.io/postcodes?lon=" + str(lon) + "&lat=" + str(lat)
+
+    url = url.replace(" ", "%20")
+
+    contents = urllib.request.urlopen(url).read()
+
+    data = json.loads(contents)
+
+    df = pd.json_normalize(data['result'])
+
+    return df
---
+++ OneDrive_1_26-09-2024/post_codes.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import urllib.request
+import json
+import sqldf
+
+def get_data_from_post_codes(post_code):
+    
+    url = "https://api.postcodes.io/postcodes/" + post_code
+
+    url = url.replace(" ", "%20")
+
+    contents = urllib.request.urlopen(url).read()
+
+    data = json.loads(contents)
+
+    df = pd.json_normalize(data['result'])
+
+    return df
