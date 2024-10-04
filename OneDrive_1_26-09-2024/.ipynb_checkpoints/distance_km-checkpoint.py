ac9d6b86863e4de95f786414eed7b7eb0ff09622
abhishekgupta3nbcu <abhig.ued@gmail.com>
2024-10-05 00:28:20 +0100

    Final

--- OneDrive_1_26-09-2024/.ipynb_checkpoints/distance_km-checkpoint.py
+++ OneDrive_1_26-09-2024/.ipynb_checkpoints/distance_km-checkpoint.py
@@ -5,21 +5,26 @@ import seaborn as sns
 import sqldf
 
 
-def get_distance(df):
+def get_distance(df, lon1='LAST_DELIVERY_LONGITUDE', lat1='LAST_DELIVERY_LATITUDE', lon2='LON', lat2='LAT'):
     R = 6373.0
+
+
+    rad_lon1 = 'Rad_' + lon1
+    rad_lat1 = 'Rad_' + lat1
+    rad_lon2 = 'Rad_' + lon2
+    rad_lat2 = 'Rad_' + lat2
+
+    new_data = {rad_lon1: np.radians(df[lon1]), rad_lat1: np.radians(df[lat1]), rad_lon2: np.radians(df[lon2]), rad_lat2: np.radians(df[lat2])}
+    df = df.assign(**new_data)
     
-    df['Rad_LAST_DELIVERY_LATITUDE'] = np.radians(df['LAST_DELIVERY_LATITUDE'])
-    df['Rad_LAST_DELIVERY_LONGITUDE'] = np.radians(df['LAST_DELIVERY_LONGITUDE'])
-    df['Rad_LAT'] = np.radians(df['LAT'])
-    df['Rad_LON'] = np.radians(df['LON'])
     
-    df['dlong'] = df['Rad_LAST_DELIVERY_LONGITUDE'] - df['Rad_LON']
-    df['dlat'] = df['Rad_LAST_DELIVERY_LATITUDE'] - df['Rad_LAT']
+    df['dlong'] = df[rad_lon1] - df[rad_lon2]
+    df['dlat'] = df[rad_lat1] - df[rad_lat2]
     
-    df['dummy_a'] = np.sin(df['dlat']/2)**2 + np.cos(df['Rad_LAST_DELIVERY_LATITUDE'])*np.cos(df['Rad_LAT'])*np.sin(df['dlong']/2)**2
+    df['dummy_a'] = np.sin(df['dlat']/2)**2 + np.cos(df[rad_lat1])*np.cos(df[rad_lat2])*np.sin(df['dlong']/2)**2
     
     df['distance'] = R*2*np.arctan2(np.sqrt(df['dummy_a']), np.sqrt(1 - df['dummy_a']))
     
-    df.drop(['dlong', 'dlat', 'dummy_a', 'Rad_LAST_DELIVERY_LATITUDE', 'Rad_LAST_DELIVERY_LONGITUDE', 'Rad_LAT', 'Rad_LON'],axis=1,inplace=True)
+    df.drop(['dlong', 'dlat', 'dummy_a', rad_lat1, rad_lon1, rad_lat2, rad_lon2],axis=1,inplace=True)
     
     return df
--- OneDrive_1_26-09-2024/cum_histogram.py
+++ OneDrive_1_26-09-2024/cum_histogram.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import sqldf
 
 def get_cumulative_histogram(df, X='min_distance', HUE='is_delivered_2'):
+    
+    
+
     sns.ecdfplot(data=df, x=X, log_scale=True, hue = HUE)
     plt.pyplot.xlim([0.001,10000])
     plt.pyplot.xlabel('Min Distance between Delivery Point and GPS Tracking \n for each shipment (km)')
@@ -12,3 +15,4 @@ def get_cumulative_histogram(df, X='min_distance', HUE='is_delivered_2'):
     plt.pyplot.grid('show')
     plt.pyplot.title('Cumulative Distribution of Minimum Distance \n (between Delivery Long/Lat & GPS Long/Lat for each shipment)')
     plt.pyplot.show()
+    
